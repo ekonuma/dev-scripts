33872a8ba6b29bb7b602a62f4a23f281e3373b40
kima-dev <konumaeike5618@hotmail.com>
2021-07-08 15:31:48 -0300

    updated

--- install/sdkman-install.sh
+++ install/sdkman-install.sh
@@ -1,4 +1,6 @@
+sudo apt install unzip
+sudo apt install zip
 curl -s "https://get.sdkman.io" | zsh
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+source $HOME/.sdkman/bin/sdkman-init.sh
 echo "Successfully installed"
 sdk version
