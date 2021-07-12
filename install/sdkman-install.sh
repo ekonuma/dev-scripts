effba4e0e87a57281cb81a9b565a40c0ab2cfa7c
kima-dev <konumaeike5618@hotmail.com>
2021-07-12 15:21:36 -0300

    changed

--- install/sdkman-install.sh
+++ install/sdkman-install.sh
@@ -1,6 +1,10 @@
+#Install SDKMan
+
+#Install necessary packages 
 sudo apt install unzip
 sudo apt install zip
+
+#Download and run in zsh
 curl -s "https://get.sdkman.io" | zsh
 source $HOME/.sdkman/bin/sdkman-init.sh
-echo "Successfully installed"
 sdk version
--- install/zsh-with-spaceship-install.sh
+++ install/zsh-with-spaceship-install.sh
@@ -1,22 +1,28 @@
+#Install ZSH
 echo "*** Start Zsh install... ***"
 sudo apt install zsh
-echo "*** Successfully installed! ***"
 zsh --version
+
+#Install Oh-My-ZSH
 echo "*** Start Oh My Zsh install... ***"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "*** Successfully installed! ***"
+
+#Install Spaceship theme
 echo "*** Start spaceship setup... ***"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+#Setting preferences
 zshrc=~/.zshrc
 zshtheme="spaceship"
 zshplugin=(git jenv spring ng node npm mvn docker gcloud)
-sed "s/^ZSH_THEME=.*/ZSH_THEME=${zshtheme}/" $zshrc
-sed "s/^plugins=.*/plugins=${zshplugin}/" $zshrc
-echo SPACESHIP_USER_SHOW=always \
-SPACESHIP_PROMPT_ADD_NEWLINE=false \
-SPACESHIP_CHAR_SYMBOL="❯" \
-SPACESHIP_CHAR_SUFFIX=" " \ 
-SPACESHIP_GCLOUD_SHOW=false >> $zshrc
-echo "*** Spaceship config completed! ***"
+
+sed -i "s/^ZSH_THEME=.*/ZSH_THEME=${zshtheme}/" $zshrc
+sed -i "s/^plugins=.*/plugins=${zshplugin}/" $zshrc
+echo SPACESHIP_USER_SHOW=always >> $zshrc
+echo SPACESHIP_PROMPT_ADD_NEWLINE=false >> $zshrc
+echo SPACESHIP_CHAR_SYMBOL="❯" >> $zshrc
+echo SPACESHIP_CHAR_SUFFIX=" " >> $zshrc
+echo SPACESHIP_GCLOUD_SHOW=false >> $zshrc
+
 echo "*** script finished. ***"
