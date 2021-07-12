cb4bbf586c24612e4baf6a1caffb2ced79684d7e
kima-dev <konumaeike5618@hotmail.com>
2021-07-12 17:28:31 -0300

    updated

--- install/zsh/zsh-with-spaceship-install.sh
+++ install/zsh/zsh-with-spaceship-install.sh
@@ -4,6 +4,7 @@ sudo apt install git
 #Instaill ZSH
 echo "*** Start Zsh install... ***\n"
 sudo apt install zsh
+curl -O https://raw.githubusercontent.com/kima-dev/dev-scripts/main/install/zsh/.zshrc --output ~/
 zsh --version
 
 #Install Oh-My-ZSH
@@ -14,14 +15,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 echo "\n*** Start spaceship setup... ***\n"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-#Setting preferences
-sed -i "s/^ZSH_THEME=.*/ZSH_THEME=spaceship/" ~/.zshrc
-sed -i "s/^plugins=.*/plugins=(git jenv spring ng node npm mvn docker gcloud)/" ~/.zshrc
-echo SPACESHIP_USER_SHOW=always >> ~/.zshrc
-echo SPACESHIP_PROMPT_ADD_NEWLINE=false >> ~/.zshrc
-echo SPACESHIP_CHAR_SYMBOL="❯" >> ~/.zshrc
-echo SPACESHIP_CHAR_SUFFIX=" " >> ~/.zshrc
-echo SPACESHIP_GCLOUD_SHOW=false >> ~/.zshrc
-
-echo "\n*** script finished. ***"
