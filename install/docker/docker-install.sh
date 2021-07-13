afba1864f4b783d9c2c49d41829f6a723018b6ae
kimadev <konumaeike5618@hotmail.com>
2021-07-13 09:47:05 -0300

    docker install script updated

--- install/docker/docker-install.sh
+++ install/docker/docker-install.sh
@@ -1,8 +1,10 @@
 #Install Docker
 #Update package
+echo "\n***Packaged update ***\n"
 sudo apt-get update
 
 #Install dependecies
+echo "\nDependencies install\n"
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -11,6 +13,7 @@ sudo apt-get install \
     lsb-release
 
 #Add official GPG Key
+echo "\nAdd Docker Official GPG Keys\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
@@ -18,8 +21,10 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 #Install docker
+echo "\nInstall docker\n"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo service docker start
 sudo docker run hello-world
 
-echo "Script finished."
+echo "\nScript finished."
