b6a6cbe0ec563f2121267644c10507278c13e2ba
小沼エイキ ー Eike Konuma <konumaeike5618@hotmail.com>
2021-08-20 21:41:34 -0300

    Update sdkman-install.sh

--- install/sdkman/sdkman-install.sh
+++ install/sdkman/sdkman-install.sh
@@ -6,5 +6,7 @@ sudo apt install zip
 
 #Download and run in zsh
 curl -s "https://get.sdkman.io" | zsh
+sudo su
+source /root/.sdkman/bin/sdkman-init.sh
 source $HOME/.sdkman/bin/sdkman-init.sh
 sdk version
